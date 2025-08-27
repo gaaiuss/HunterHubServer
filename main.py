6400844cf4950dc9a7e3417d6e315b554b9980ce
gaaiuss <caio.gui.castro@gmail.com>
2025-08-27 11:30:13 -0300

    web scraping

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # Tutorial https://www.youtube.com/watch?v=Qd8JT0bnJGs
+import re
+
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://localhost:8000/'
 response = requests.get(url)
+raw_html = response.text
+parsed_html = BeautifulSoup(raw_html, 'html.parser')
 
-print(response.status_code)
-# print(response.headers)
-# print(response.content)
-# print(response.text)
+# if parsed_html.title is not None:
+# print(parsed_html.title.text)
+
+the_apex_title = parsed_html.select_one('#apex > div > h2')
+if the_apex_title is not None:
+    # print(the_apex_title.text)
+    article = the_apex_title.parent
+    if article is not None:
+        for p in article.select('p'):
+            print(re.sub(r'\s{1}', ' ', p.text).strip())
